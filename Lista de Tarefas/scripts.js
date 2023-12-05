cfd81fd2a02a2a26870b144fc2fbe577501239df
antuneseds <esterdossantosantunes04@gamil.com>
2023-12-05 17:37:18 -0300

    Lista de Tarefas

--- Lista de Tarefas/scripts.js
+++ Lista de Tarefas/scripts.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('.botao-add')
 const input = document.querySelector('.input-task')
+const listaCompleta = document.querySelector('.list')
 
 
 let minhaListaDeItens = []
@@ -7,8 +8,28 @@ let minhaListaDeItens = []
 
 function adicionarNovaTarefa() {
     minhaListaDeItens.push(input.value)
+    
+    mostrarTarefas()
+}
 
-    console.log(minhaListaDeItens)
+function mostrarTarefas() {
+
+    let novaLi = ''
+
+    minhaListaDeItens.forEach((tarefa) => {
+        novaLi = novaLi + `
+
+        <li class="task" >
+            <img src="./img/check.png" alt="check">
+            <p>${tarefa}</p>
+            <img src="./img/lixeira.png" alt="lixeira">
+        </li>
+
+         `
+    }) 
+
+    listaCompleta.innerHTML = novaLi
+
 }
 
 
